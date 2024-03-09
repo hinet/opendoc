906bc745e682c591a9225ce0f53d1597ddc58bd0
hinet <63603636@qq.com>
2024-03-09 13:32:15 +0800

    Add layout

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,7 @@
-<script setup lang="ts">
-import Editor from '@/components/editor/Index.vue'
-import { ref } from 'vue'
-const people = [
-  {
-    name: 'Calvin Hawkins',
-    email: 'calvin.hawkins@example.com',
-    image:
-      'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
-  },
-  {
-    name: 'Kristen Ramos',
-    email: 'kristen.ramos@example.com',
-    image:
-      'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
-  },
-  {
-    name: 'Ted Fox',
-    email: 'ted.fox@example.com',
-    image:
-      'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
-  }
-]
-const value = ref('')
-const maxLength = ref(300)
-const handleChange = () => {}
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <ul class="divide-y divide-gray-200">
-    <li v-for="person in people" :key="person.email" class="py-4 flex">
-      <img class="h-10 w-10 rounded-full" :src="person.image" alt="" />
-      <div class="ml-3">
-        <p class="text-sm font-medium text-gray-900">{{ person.name }}</p>
-        <p class="text-sm text-gray-500">{{ person.email }}</p>
-      </div>
-    </li>
-  </ul>
-  <button class="bg-blue-600 text-white py-2 px-4 rounded">submit</button>
-  <Editor v-model:value="value" :max-length="maxLength" @change="handleChange" />
+  <Router-view></Router-view>
 </template>
 
 <style scoped></style>
---
+++ src/components/layout/Index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="flex flex-row h-full">
+    <nav class="flex flex-col w-64 bg-gray-100 h-full">
+      <div class="h-10 flex items-center justify-center">
+        <a href="#" class="text-2xl font-bold text-gray-900">Menu</a>
+      </div>
+      <div class="flex-grow">
+        <ul class="py-4">
+          <li class="mb-2"><a href="#" class="block p-2 rounded hover:bg-gray-200">首页</a></li>
+          <li class="mb-2">
+            <a href="#" class="block p-2 rounded hover:bg-gray-200">关于我们</a>
+          </li>
+          <li class="mb-2"><a href="#" class="block p-2 rounded hover:bg-gray-200">服务</a></li>
+          <li class="mb-2">
+            <a href="#" class="block p-2 rounded hover:bg-gray-200">联系我们</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <div class="container px-6">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style lang="less" scoped></style>
