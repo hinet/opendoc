a8cfc0e2dc1fa9e1344b3a5457bd0754173276ef
hinet <63603636@qq.com>
2024-03-09 12:59:10 +0800

    init project

---
+++ src/components/editor/Index.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { Editor, Viewer } from '@bytemd/vue-next'
+import gfm from '@bytemd/plugin-gfm'
+import 'bytemd/dist/index.css'
+let content = ref('')
+
+const plugins = [gfm()]
+
+const handleChange = (v: string) => {
+  console.log(v)
+  content.value = v
+}
+</script>
+<template>
+  <Editor :value="content" :plugins="plugins" @change="handleChange" />
+</template>
+<style lang="less" scoped></style>
